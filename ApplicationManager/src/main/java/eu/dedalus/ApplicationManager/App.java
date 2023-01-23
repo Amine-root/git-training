48e45efeb09dc1eeff945a54541ca6558a520557
amine.abouelfath <amine.abouelfath@dedalus.eu>
2023-01-23 20:41:06 +0100

    first commit

---
+++ ApplicationManager/src/main/java/eu/dedalus/ApplicationManager/App.java
@@ -0,0 +1,13 @@
+package eu.dedalus.ApplicationManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
